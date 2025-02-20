05fac9a4a3bd59ff6a91e0ed78ecc6ab8ecb5955
James Anderson <james.anderson@wright.edu>
2025-02-20 15:36:57 -0500

    added more examples of methods as well as JavaDoc to LetterGradeApp

--- Week6/LetterGradeApp/src/LetterGradeApp.java
+++ Week6/LetterGradeApp/src/LetterGradeApp.java
@@ -2,12 +2,13 @@
  * LetterGradeApp
  * Extension of the in-class exercise asking user for assignment
  * grades and printing out the letter grades
- * <p>
+ *
  * The methods have examples of JavaDoc, which can be auto-generated
  * if you type /** then press enter
  */
 
 import java.util.Scanner;
+
 
 public class LetterGradeApp {
     // our methods go inside of this class
@@ -48,6 +49,11 @@ public class LetterGradeApp {
         // method overloads
         double absOf = Math.abs(15.6);
         double anotherAbs = Math.abs(-2);
+
+        String javaDocTestResult = javaDocTest(9.8);
+
+        int castTest = 9;
+        int anotherTest = testMethod(9, castTest);
     }
 
     /**
@@ -55,7 +61,7 @@ public class LetterGradeApp {
      * either A, B, C, D, or F
      *
      * @param grade the integer value of the grade
-     * @return the letter grade of the input grade integer
+     * @return the letter grade as a char of the input grade integer
      */
     public static char intGradeToLetterGrade(int grade) {
         char result = 'F';
@@ -112,5 +118,75 @@ public class LetterGradeApp {
         return result;
     }
 
+    /**
+     * javaDocTests takes a double value and returns a string
+     * stating what the value is
+     *
+     * @param theValue double to be added to a string
+     * @return a String like: "The value is: " + the value
+     */
+    public static String javaDocTest(double theValue) {
+        String result = ""; // method stub
+        if (result.equals("hello")) {
+            int j = 10;
+            if (true) {
+                j = 9;
+            }
+            System.out.println(result);
+        }
+        //System.out.println(j);
+        int i;
+        for (i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println(i);
+        return result;
+    }
+
+    /**
+     * scopeTest demonstrates how variables can be accessed and where they are out of scope
+     *
+     * @param param param is local to scopeTest, it cannot be accessed outside this method
+     */
+    public static void scopeTest(int param) {
+        String result = ""; // result is local to scopeTest, it can be used anywhere in this method
+        if (result.equals("hello")) {
+            int j = 10; // j cannot be accessed outisde of this if
+            if (true) {
+                j = 9; // this if is within the scope of the outer if, so can access j
+            }
+            System.out.println(result);
+        }
+        //System.out.println(j); // cannot access j from here
+        int i; // can declare loop counter outside of for loop
+        for (i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println(i); // because i was created outside of for loop, can access outside loop
+    }
+
+    /**
+     * example of method overloading
+     * both methods are named testMethod, but they take different parameters
+     * they can also take different numbers of parameters
+     *
+     * @param i integer, first parameter
+     * @param j double, second parameter
+     * @return methods cannot be overloaded based soley on return type
+     */
+    public static int testMethod(int i, double j) {
+        return i;
+    }
+
+    /**
+     * second overload of testMethod takes one double parameter
+     *
+     * @param i double, single parameter
+     * @return double, if both test methods took the same type parameters
+     * they cannot be distinguished soley on return type
+     */
+    public static double testMethod(double i) {
+        return i;
+    }
 
 }
