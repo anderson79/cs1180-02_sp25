6e7dc6942e41b5fe80c9b8d4e705ea2a84070cdb
James Anderson <james.anderson@wright.edu>
2025-04-02 10:45:13 -0400

    added code for Point class from 4/1/2025

---
+++ Week11/objects/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+
+    public static void main(String[] args) {
+       testPoint();
+    }
+
+    public static void testPoint() {
+//        String nullString = null;
+//        nullString.toCharArray();
+
+        Point p1 = new Point(10000, 12, null);
+        Point p2 = new Point();
+        p1.display();// causes program to crash if color has not been initialized
+
+        String s = "hello"; // strings are immutable
+        String toUpper = s.toUpperCase();
+        s = s.toUpperCase(); // reassign to change a string
+        System.out.println(s);
+        System.out.println(toUpper);
+
+        s = "world";
+        System.out.println(s);
+
+        // System.out.println("p1.x: " + p1.x);
+        // System.out.println("p1.y: " + p1.y);
+
+        p1.setX(40);
+        p1.setX(-9);
+        p1.setY(8);
+        p1.setColor("red");
+
+        p2.setX(-15);
+        p2.setY(16);
+        p2.setColor("Blue");
+
+        System.out.println("p1.x: " + p1.getX());
+        // System.out.println("p1.y: " + p1.y);
+        // System.out.println("p1.color: " + p1.color);
+
+        System.out.println("In main method: ");
+        p1.display();
+        p2.display();
+
+        anotherMethod();
+
+        System.out.println("In main method: ");
+        p1.display();
+        p2.display();
+    }
+
+    public static void anotherMethod() {
+        Point p1 = new Point();
+        p1.setX(23);
+        p1.setY(42);
+        p1.setColor("green");
+        System.out.println("In anotherMethod: ");
+        p1.display();
+
+    }
+}
---
+++ Week11/objects/src/Point.java
@@ -0,0 +1,98 @@
+public class Point {
+    // fields
+    private int x;
+    private int y;
+    private String color; // initialzied to null
+
+    // default constructor
+    Point() {
+        this.x = 0;
+        this.y = 0;
+        this.color = "gray";
+    }
+
+    Point(int newX, int y, String color) {
+        setX(newX);
+        setY(y);
+        if (color == null) {
+            this.color = "gray";
+        } else {
+            setColor(color);
+        }
+    }
+
+    // methods
+    public void display() {
+        int z = this.color.length();
+        System.out.println("{" + x + ", " + y + "}: " + color);
+    }
+
+    /**
+     * helper method to make sure value is between 0 and max
+     *
+     * @param value value to check if is in range
+     * @param max   the max the value should be
+     * @return true if value is within range, false otherwise
+     */
+    private boolean inRange(int value, int max) {
+        boolean result = false;
+        if (value > max) {
+            result = false;
+        } else if (value < 0) {
+            result = false;
+        } else {
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * mutator (setter) method to change x field
+     * x must be between 0 and 100
+     *
+     * @param newX the new value of x
+     */
+    public void setX(int x) {
+        if (inRange(x, 100)) {
+            this.x = x;
+        }
+    }
+
+    /**
+     * mutator (setter) method to change y field
+     *
+     * @param newY the new value of y
+     */
+    public void setY(int newY) {
+        if (inRange(newY, 80)) {
+            this.y = newY;
+        }
+    }
+
+    public void setColor(String newColor) {
+        this.color = newColor.toLowerCase();
+    }
+
+    /**
+     * accessor (getter) method for x
+     *
+     * @return value of x
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * accessor (getter) method for y
+     *
+     * @return the value of y
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+} // end of Point class
