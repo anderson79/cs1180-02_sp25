5676ff0300c70545b5c05599dd6412b98511327a
James Anderson <james.anderson@wright.edu>
2025-04-10 15:34:28 -0400

    added updates to Student class including canGraduate, toString, and static field studentCount

--- Week12/StudentClass/src/Main.java
+++ Week12/StudentClass/src/Main.java
@@ -1,14 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
+        Student.studentCount++;
         s1.setGPA(100);
         s1.setGPA(-100);
-        s1.setGPA(2.5);
-        Student s2 = new Student("James A", 45, 12345678, 4.0);
+        s1.setGPA(2.0);
 
-        s1.setAge(-1000);
-        s1.setAge(45);
+        Student s2 = new Student("James A", -45, 12345678, 4.0);
+        Student.studentCount++;
+        s2.setAge(-1000);
+        s2.setAge(45);
+        System.out.println("s2.isAlumni(): " + s2.isAlumni());
 
         String myName = s2.getName();
+
+        System.out.println("s1.canGraduate(): " + s1.canGraduate());
+        System.out.println("s2.canGraduate(): " + s2.canGraduate());
+
+        System.out.println("s1.isAlumni(): " + s1.isAlumni());
+        System.out.println("s2.isAlumni(): " + s2.isAlumni());
+
+        int myInt = 7;
+        System.out.println(myInt);
+        String myString = "hello";
+        System.out.println(myString.toString());
+
+        System.out.println(s1);
+        System.out.println(s2);
+
     }
 }
--- Week12/StudentClass/src/Student.java
+++ Week12/StudentClass/src/Student.java
@@ -4,6 +4,10 @@ public class Student {
     private int uid;
     private int age;
     private double gpa;
+    private boolean alumni;
+
+    // static fields are shared by all instances of the class
+    public static int studentCount;
 
     // constructors
     // no-arg default constructor
@@ -12,14 +16,20 @@ public class Student {
         this.age = -1;
         this.uid = -1;
         this.gpa = -1.0;
+        this.alumni = false;
     }
 
     // parameterized constructor
     Student(String name, int age, int uid, double gpa) {
         this.name = name;
-        this.age = age;
+        this.setAge(age);
         this.uid = uid;
-        this.gpa = gpa;
+        this.setGPA(gpa);
+        this.canGraduate();
+    }
+
+    public boolean isAlumni() {
+        return alumni;
     }
 
     // getters/setters
@@ -36,11 +46,36 @@ public class Student {
     public void setAge(int newAge) {
         if (newAge > 0) {
             this.age = newAge;
+        } else {
+            this.age = -1;
         }
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean canGraduate() {
+        if (this.gpa >= 2.5) {
+            this.alumni = true;
+            return this.alumni;
+        }
+
+//        if (gpa >= 2.5) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+
+//        if (gpa >= 2.5) {
+//            return true;
+//        }
+        return false;
+    }
+
+    public String toString() {
+        String result = "Student: " + name + " UID: " + uid + " GPA: " + gpa + " age: " + age + " alumni: " + alumni + " studentCount: " + studentCount;
+        return result;
     }
 
 } // end class Student
